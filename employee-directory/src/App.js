2f2330708125672ac5915fd8a7367687e55a90fd
Ari Horowitz <ariehh@comcast.net>
2020-04-09 14:08:45 -0700

    table and filternav rendering in app.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import FilterNav from "./components/FilterNav";
+import Table from "./components/Table";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <FilterNav />
+      <Table />
     </div>
   );
 }
---
+++ employee-directory/src/components/Table/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Table() {
+  return <p>Table</p>;
+}
+
+export default Table;
